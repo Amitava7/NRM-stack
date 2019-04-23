541cce3b5ea8763908809723a48630c1db300284
amitava <amitava.mozumder@citrix.com>
2019-04-23 11:22:02 +0530

    google oauth part 1

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send({ami: "tava"});
+  passport.use(new GoogleStrategy({
+    clientID: process.env.googleClientID,
+    clientSecret: process.env.googleSecret,
+    callbackURL: '/auth/google/callback'
+  }, accessToken => {
+    console.log(accessToken);
+  }));
 
-});
+  app.get('/',(req,res)=>{
+    res.send({ami: "tava"});
 
-const PORT = process.env.PORT || 5000;
+  });
+  app.get('/auth/google', passport.authenticate('google',{
+    scope: ['profile','email']
+  }));
 
-app.listen(PORT);
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT);
