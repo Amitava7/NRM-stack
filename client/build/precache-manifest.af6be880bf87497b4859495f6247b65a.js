4ade4d9304decc7baa03c8bbe781c77d1ce58bc2
Amitava7 <amitavamozumder@gmail.com>
2022-01-17 02:05:35 +0530

    email updated

--- client/build/precache-manifest.af6be880bf87497b4859495f6247b65a.js
+++ client/build/precache-manifest.af6be880bf87497b4859495f6247b65a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8fec96b31f5f4724d276b6f81d87839",
+    "revision": "e2efb746b86e35f9f60fc9deea8a34e9",
     "url": "/index.html"
   },
   {
-    "revision": "11f984fb338a102b6820",
-    "url": "/static/css/main.75797bef.chunk.css"
+    "revision": "6f9ffe1894e1e015cbde",
+    "url": "/static/css/main.f24f1a2a.chunk.css"
   },
   {
     "revision": "630579a84fdc0e9a3f94",
     "url": "/static/js/2.a6d9e4bc.chunk.js"
   },
   {
-    "revision": "11f984fb338a102b6820",
-    "url": "/static/js/main.3060c3f9.chunk.js"
+    "revision": "6f9ffe1894e1e015cbde",
+    "url": "/static/js/main.c31ba107.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
