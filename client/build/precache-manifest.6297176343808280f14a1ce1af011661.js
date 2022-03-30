42822235786cc0a4e4fbe441469eedcef43db5a9
Amitava7 <amitavamozumder@gmail.com>
2022-03-30 23:41:53 +0530

    bundle update

---
+++ client/build/precache-manifest.6297176343808280f14a1ce1af011661.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e056f1f84a8821f1f8c54727755d2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c90b18887a5889faab",
+    "url": "/static/css/3.dac9d945.chunk.css"
+  },
+  {
+    "revision": "d87dadd83325f1c9be34",
+    "url": "/static/css/4.c87a57bf.chunk.css"
+  },
+  {
+    "revision": "af47865389d77cbb9ec7",
+    "url": "/static/css/5.57131981.chunk.css"
+  },
+  {
+    "revision": "595433a01ed7ba1712c4",
+    "url": "/static/css/6.4ec964ad.chunk.css"
+  },
+  {
+    "revision": "8034aaac9f75f300876f",
+    "url": "/static/css/7.e4f8b1bc.chunk.css"
+  },
+  {
+    "revision": "530074d3ebbc3ee5ec14",
+    "url": "/static/css/main.7a1701cc.chunk.css"
+  },
+  {
+    "revision": "14cf66ecd7de35ee376c",
+    "url": "/static/js/2.8fdb8d1a.chunk.js"
+  },
+  {
+    "revision": "31c90b18887a5889faab",
+    "url": "/static/js/3.54bb7a96.chunk.js"
+  },
+  {
+    "revision": "d87dadd83325f1c9be34",
+    "url": "/static/js/4.8a4fbce1.chunk.js"
+  },
+  {
+    "revision": "af47865389d77cbb9ec7",
+    "url": "/static/js/5.a1c78bc3.chunk.js"
+  },
+  {
+    "revision": "595433a01ed7ba1712c4",
+    "url": "/static/js/6.a704f41e.chunk.js"
+  },
+  {
+    "revision": "8034aaac9f75f300876f",
+    "url": "/static/js/7.d3e3d2a1.chunk.js"
+  },
+  {
+    "revision": "530074d3ebbc3ee5ec14",
+    "url": "/static/js/main.907aae94.chunk.js"
+  },
+  {
+    "revision": "e6f5a64ff2fa17639616",
+    "url": "/static/js/runtime~main.b64e0433.js"
+  }
+]);
